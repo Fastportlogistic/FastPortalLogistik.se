22b196767429c02d9d6401446c25b5d8f4d8f10e
FastPortalLogistik <Abdisalanmahad5@gmail.com>
2025-04-29 13:58:37 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+// Enkel interaktivitet - t.ex. en alert när sidan laddas
+
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('FastPorta Logistics hemsida är redo!');
+});
